fce0ea5c2cd25d3ed2b0ef2db8a1ad959740f221
minato <lucaspatrickr5@gmail.com>
2023-10-31 18:02:18 -0300

    newupdate

---
+++ src/SequencialContaIdGeneratorStrategy.java
@@ -0,0 +1,19 @@
+import java.io.Serializable;
+
+
+public class SequencialContaIdGeneratorStrategy implements idGeneratorStrategy, Serializable{
+
+    private int prox_num;
+
+    public SequencialContaIdGeneratorStrategy() {
+        this.prox_num = 1;
+
+    }
+
+    public String nextId() { //adiciana o proximo idex para o prox_num + 1
+        return String.valueOf(prox_num++);
+    }
+    
+}
+//teste
+//teste
---
+++ src/idGeneratorStrategy.java
@@ -0,0 +1,6 @@
+public interface idGeneratorStrategy {
+
+        public String nextId();
+
+    
+}
